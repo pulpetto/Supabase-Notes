4c7fe7e48b44e8444ec7f52d7fa720a3035901a7
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-23 15:46:40 +0200

    initialize supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'notes_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://oelswofmtfqqpmyukqxi.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9lbHN3b2ZtdGZxcXBteXVrcXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2NzczODQsImV4cCI6MjA2NjI1MzM4NH0.UYqJppI2pnfLliuCCUBWSazmPx7Sh6W1-n816jBu8qk',
+  );
+
   runApp(const MyApp());
 }
 
@@ -9,6 +18,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const Scaffold());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: NotesPage(),
+    );
   }
 }
