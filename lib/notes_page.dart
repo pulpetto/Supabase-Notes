389ae6c5beb1f1a5b0876444fb3323de066ed167
pulpetto <116948697+pulpetto@users.noreply.github.com>
2025-06-23 17:04:57 +0200

    saving notes functionality

--- lib/notes_page.dart
+++ lib/notes_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class NotesPage extends StatefulWidget {
   const NotesPage({super.key});
@@ -8,8 +9,41 @@ class NotesPage extends StatefulWidget {
 }
 
 class _NotesPageState extends State<NotesPage> {
+  final textController = TextEditingController();
+
+  void addNewNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              saveNote();
+              Navigator.pop(context);
+            },
+            child: const Text('Save'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void saveNote() async {
+    await Supabase.instance.client
+        .from('notes')
+        .insert({'body': textController.text});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: addNewNote,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
